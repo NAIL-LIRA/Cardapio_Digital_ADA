4d7ab4f6ae39e23c69fa456ee33a6aa9ea28ce48
Fabyo Kock <fabyokock@gmail.com>
2026-09-17 08:30:54 -0300

    Início do Projeto Cardápio Digital

---
+++ src/produto.ts
@@ -0,0 +1,24 @@
+// src/Produto.ts
+
+export class Produto {
+  constructor(
+    public id: number,
+    public nome: string,
+    public preco: number,
+    public descricao: string,
+    public imagemUrl: string,
+  ) {}
+
+  // Retorna o HTML do card visual do produto
+  gerarHTML(): string {
+    return `
+  <div class="card-produto">
+      <img src="${this.imagemUrl}" alt="${this.nome}"> 
+      <div class="card-conteudo">
+          <h3>${this.nome}</h3>
+          <p class="descricao">${this.descricao}<p/>
+          <p class="preco">${this.preco.toFixed(2)}</p>
+      </div> 
+  </div>`;
+  }
+}
